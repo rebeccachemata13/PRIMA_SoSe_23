ed8410741de45d403e6f46f095fc24b2b59a20f5
Rebecca Chemata <80950540+rebeccachemata13@users.noreply.github.com>
2022-10-13 00:21:02 +0200

    Luigi runs but wrong position and strange SpriteSheet

--- Luigi/Script/Build/Script.js
+++ Luigi/Script/Build/Script.js
@@ -1,1 +1,92 @@
-console.warn("%cFor scripting, start your favorite IDE (e.g. VSCode) with typescript installed, load and compile this project", "color: red; font-weight: bold;");
+"use strict";
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Project.registerScriptNamespace(Script); // Register the namespace to FUDGE for serialization
+    class CustomComponentScript extends ƒ.ComponentScript {
+        // Register the script as component for use in the editor via drag&drop
+        static iSubclass = ƒ.Component.registerSubclass(CustomComponentScript);
+        // Properties may be mutated by users in the editor via the automatically created user interface
+        message = "CustomComponentScript added to ";
+        constructor() {
+            super();
+            // Don't start when running in editor
+            if (ƒ.Project.mode == ƒ.MODE.EDITOR)
+                return;
+            // Listen to this component being added to or removed from a node
+            this.addEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
+            this.addEventListener("nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */, this.hndEvent);
+        }
+        // Activate the functions of this component as response to events
+        hndEvent = (_event) => {
+            switch (_event.type) {
+                case "componentAdd" /* ƒ.EVENT.COMPONENT_ADD */:
+                    ƒ.Debug.log(this.message, this.node);
+                    break;
+                case "componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */:
+                    this.removeEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
+                    this.removeEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
+                    break;
+                case "nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */:
+                    // if deserialized the node is now fully reconstructed and access to all its components and children is possible
+                    break;
+            }
+        };
+    }
+    Script.CustomComponentScript = CustomComponentScript;
+})(Script || (Script = {}));
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    var ƒAid = FudgeAid;
+    ƒ.Debug.info("Main Program Template running!");
+    let viewport;
+    // Add eventlistener for the loading of the window
+    //window.addEventListener("load", onLoad);  
+    //window.addEventListener("interactiveViewportStarted", onViewportStart)
+    // Define luigiSpriteNode from FUDGE
+    let luigiSpriteNode;
+    let luigi;
+    // load Handler
+    document.addEventListener("interactiveViewportStarted", start);
+    async function start(_event) {
+        viewport = _event.detail;
+        let graph = viewport.getBranch();
+        luigi = graph.getChildrenByName("LuigiPosition")[0].getChildrenByName("Luigi")[0];
+        console.log("branch" + graph.name);
+        console.log("luigi" + luigi.name);
+        luigiSpriteNode = await createluigiSprite();
+        luigi.addChild(luigiSpriteNode);
+        luigi.getComponent(ƒ.ComponentMaterial).activate(false);
+        ƒ.Loop.addEventListener("loopFrame" /* ƒ.EVENT.LOOP_FRAME */, update);
+        // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+        // edit framerate here
+        ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, 10);
+    }
+    function update(_event) {
+        // ƒ.Physics.simulate();  // if physics is included and used
+        viewport.draw();
+        ƒ.AudioManager.default.update();
+    }
+    async function createluigiSprite() {
+        // load spritesheet from folder and add a "coat" to it.
+        let luigiSpriteSheet = new ƒ.TextureImage();
+        await luigiSpriteSheet.load("modern_luigi_sprite_sheet_by_mbf1000_d86t2ex.png");
+        let coat = new ƒ.CoatTextured(undefined, luigiSpriteSheet);
+        // add running animation
+        let luigiAnimation = new ƒAid.SpriteSheetAnimation("luigi_Run", coat);
+        luigiAnimation.generateByGrid(ƒ.Rectangle.GET(0, 56, 20, 50), 10, 40, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(40));
+        luigiSpriteNode = new ƒAid.NodeSprite("luigi_Sprite");
+        // adds a transform component to the sprite
+        luigiSpriteNode.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
+        luigiSpriteNode.setAnimation(luigiAnimation);
+        // play animation forwards
+        luigiSpriteNode.setFrameDirection(1);
+        // wohl unnötig?
+        luigiSpriteNode.mtxLocal.translateY(-1);
+        //set framerate here
+        luigiSpriteNode.framerate = 8;
+        return luigiSpriteNode;
+    }
+})(Script || (Script = {}));
