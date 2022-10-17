62fb6714fcbbb293ba691becc19285bbb0ab5ddd
rebeccachemata13 <80950540+rebeccachemata13@users.noreply.github.com>
2022-10-17 18:21:10 +0200

    Declaring different Animation Types

--- Luigi/Script/Source/Main.ts
+++ Luigi/Script/Source/Main.ts
@@ -5,21 +5,17 @@ namespace Script {
 
   let viewport: ƒ.Viewport;
 
-  // Add eventlistener for the loading of the window
-  //window.addEventListener("load", onLoad);  
-  //window.addEventListener("interactiveViewportStarted", onViewportStart)
-
-  // Define luigiSpriteNode from FUDGE
   let luigiSpriteNode: ƒAid.NodeSprite;
-
   let luigi: ƒ.Node;
 
-  // load Handler
+  let luigiStandingAnimation: ƒAid.SpriteSheetAnimation;
+  let luigiRunningAnimation: ƒAid.SpriteSheetAnimation;
+  let luigiJumpingAnimation: ƒAid.SpriteSheetAnimation;
 
+  
   document.addEventListener("interactiveViewportStarted", <EventListener><unknown>start);
 
   async function start(_event: CustomEvent): Promise<void> {
-    // _event.detail IST der viewport. deshalb können wir das so zuweisen
     viewport = _event.detail;
 
     let graph: ƒ.Node = viewport.getBranch();
@@ -30,9 +26,7 @@ namespace Script {
     luigi.getComponent(ƒ.ComponentMaterial).activate(false);
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
 
-    // edit framerate here
     ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, 10);
   }
   
@@ -45,34 +39,26 @@ namespace Script {
   }
 
   async function createluigiSprite(): Promise<ƒAid.NodeSprite> {
-
-    // load spritesheet from folder and add a "coat" to it.
+    
     let luigiSpriteSheet: ƒ.TextureImage = new ƒ.TextureImage();
     await luigiSpriteSheet.load("modern_luigi_sprite_sheet_by_mbf1000_d86t2ex.png");
     let coat: ƒ.CoatTextured = new ƒ.CoatTextured(undefined, luigiSpriteSheet);
-    
-    // add running animation
+
     let luigiAnimation: ƒAid.SpriteSheetAnimation = new ƒAid.SpriteSheetAnimation("luigi_Run", coat);
     luigiAnimation.generateByGrid(ƒ.Rectangle.GET(10, 60, 20, 45), 8, 50, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(20));
     
 
     luigiSpriteNode = new ƒAid.NodeSprite("luigi_Sprite");
-
-    // adds a transform component to the sprite
     luigiSpriteNode.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
     luigiSpriteNode.setAnimation(luigiAnimation);
-    // play animation forwards
     luigiSpriteNode.setFrameDirection(1);
-
-    // wohl unnötig?
+    
     luigiSpriteNode.mtxLocal.translateY(0.35);
     luigiSpriteNode.mtxLocal.translateX(0);
     luigiSpriteNode.mtxLocal.translateZ(1);
     luigiSpriteNode.mtxLocal.scaleX(1.75);
     luigiSpriteNode.mtxLocal.scaleY(2);
 
-
-    //set framerate here
     luigiSpriteNode.framerate = 12;
 
     return luigiSpriteNode;
