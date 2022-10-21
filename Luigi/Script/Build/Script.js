9fe9fda4f050176864418895857bbaf1c58ce942
rebeccachemata13 <80950540+rebeccachemata13@users.noreply.github.com>
2022-10-21 18:15:26 +0200

    Cleaner Code, Luigi jumps alone and sprints perfectly now

--- Luigi/Script/Build/Script.js
+++ Luigi/Script/Build/Script.js
@@ -14,21 +14,21 @@ var Script;
             if (ƒ.Project.mode == ƒ.MODE.EDITOR)
                 return;
             // Listen to this component being added to or removed from a node
-            this.addEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
-            this.addEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
-            this.addEventListener("nodeDeserialized" /* NODE_DESERIALIZED */, this.hndEvent);
+            this.addEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
+            this.addEventListener("nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */, this.hndEvent);
         }
         // Activate the functions of this component as response to events
         hndEvent = (_event) => {
             switch (_event.type) {
-                case "componentAdd" /* COMPONENT_ADD */:
+                case "componentAdd" /* ƒ.EVENT.COMPONENT_ADD */:
                     ƒ.Debug.log(this.message, this.node);
                     break;
-                case "componentRemove" /* COMPONENT_REMOVE */:
-                    this.removeEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
-                    this.removeEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+                case "componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */:
+                    this.removeEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
+                    this.removeEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
                     break;
-                case "nodeDeserialized" /* NODE_DESERIALIZED */:
+                case "nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */:
                     // if deserialized the node is now fully reconstructed and access to all its components and children is possible
                     break;
             }
@@ -42,41 +42,43 @@ var Script;
     var ƒAid = FudgeAid;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
+    document.addEventListener("interactiveViewportStarted", start);
     //LuigiSprite
     let luigiSpriteNode;
     let luigi;
-    //Speed and Direction Variables
-    let walkSpeed = 2;
-    let leftDirection;
-    let gravity = 5;
+    async function start(_event) {
+        viewport = _event.detail;
+        luigiNodeInit(_event);
+    }
     //Sprite Animations
     let luigiWalkAnimation;
     let luigiRunAnimation;
-    document.addEventListener("interactiveViewportStarted", start);
-    async function start(_event) {
-        viewport = _event.detail;
+    async function initAnimations(coat) {
+        luigiWalkAnimation = new ƒAid.SpriteSheetAnimation("luigi_walk", coat);
+        luigiWalkAnimation.generateByGrid(ƒ.Rectangle.GET(10, 60, 20, 45), 8, 50, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(20));
+        luigiRunAnimation = new ƒAid.SpriteSheetAnimation("luigi_run", coat);
+        luigiRunAnimation.generateByGrid(ƒ.Rectangle.GET(8, 245, 37, 45), 2, 50, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(40));
+    }
+    async function update(_event) {
+        //luigiFall();
+        luigiControls();
+        viewport.draw();
+        ƒ.AudioManager.default.update();
+    }
+    async function luigiNodeInit(_event) {
         let graph = viewport.getBranch();
         luigi = graph.getChildrenByName("LuigiPosition")[0].getChildrenByName("Luigi")[0];
         luigi.getComponent(ƒ.ComponentMaterial).activate(false);
         luigiSpriteNode = await createluigiSprite();
         luigi.addChild(luigiSpriteNode);
-        luigiFall();
-        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        ƒ.Loop.addEventListener("loopFrame" /* ƒ.EVENT.LOOP_FRAME */, update);
         ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, 10);
-    }
-    async function update(_event) {
-        luigiControls();
-        viewport.draw();
-        ƒ.AudioManager.default.update();
     }
     async function createluigiSprite() {
         let luigiSpriteSheet = new ƒ.TextureImage();
         await luigiSpriteSheet.load("modern_luigi_sprite_sheet_by_mbf1000_d86t2ex.png");
         let coat = new ƒ.CoatTextured(undefined, luigiSpriteSheet);
-        luigiWalkAnimation = new ƒAid.SpriteSheetAnimation("luigi_walk", coat);
-        luigiWalkAnimation.generateByGrid(ƒ.Rectangle.GET(10, 60, 20, 45), 8, 50, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(20));
-        luigiRunAnimation = new ƒAid.SpriteSheetAnimation("luigi_run", coat);
-        luigiRunAnimation.generateByGrid(ƒ.Rectangle.GET(8, 245, 37, 45), 2, 50, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(40));
+        initAnimations(coat);
         luigiSpriteNode = new ƒAid.NodeSprite("luigi_Sprite");
         luigiSpriteNode.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
         luigiSpriteNode.setAnimation(luigiWalkAnimation);
@@ -89,40 +91,70 @@ var Script;
         luigiSpriteNode.framerate = 12;
         return luigiSpriteNode;
     }
+    //Speed and Direction Variables
+    const xSpeedDefault = .9;
+    const xSpeedSprint = 5;
+    let ySpeed = 0.1;
+    let gravity = 0.05;
+    let leftDirection;
+    let prevSprint = false;
     async function luigiControls() {
-        let distance = walkSpeed * ƒ.Loop.timeFrameGame / 1000;
+        let deltaTime = ƒ.Loop.timeFrameGame / 1000;
+        ySpeed -= gravity * deltaTime;
+        luigiSpriteNode.mtxLocal.translateY(ySpeed);
+        let pos = luigiSpriteNode.mtxLocal.translation;
+        if (pos.y + ySpeed > 0) {
+            luigiSpriteNode.mtxLocal.translateY(ySpeed);
+        }
+        else {
+            ySpeed = 0;
+            pos.y = 0;
+            luigiSpriteNode.mtxLocal.translation = pos;
+        }
+        let walkSpeed = xSpeedDefault;
+        if (leftDirection) {
+            walkSpeed = -xSpeedDefault;
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SHIFT_LEFT, ƒ.KEYBOARD_CODE.SHIFT_RIGHT])) {
+            walkSpeed = xSpeedSprint;
+            if (leftDirection) {
+                walkSpeed = -xSpeedSprint;
+            }
+        }
+        const xTranslation = walkSpeed * deltaTime;
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT])) {
-            luigiSpriteNode.mtxLocal.translateX(-distance);
+            luigiSpriteNode.mtxLocal.translateX(-xTranslation);
             leftDirection = true;
-            if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SHIFT_LEFT, ƒ.KEYBOARD_CODE.SHIFT_RIGHT])) {
-                walkSpeed = -5;
-                luigiSpriteNode.setAnimation(luigiRunAnimation);
+            if (walkSpeed < -1) {
+                if (!prevSprint) {
+                    prevSprint = true;
+                    luigiSpriteNode.setAnimation(luigiRunAnimation);
+                }
             }
             else {
-                walkSpeed = -2;
+                prevSprint = false;
             }
         }
         else if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT])) {
-            luigiSpriteNode.mtxLocal.translateX(distance);
+            luigiSpriteNode.mtxLocal.translateX(xTranslation);
             leftDirection = false;
-            if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SHIFT_LEFT, ƒ.KEYBOARD_CODE.SHIFT_RIGHT])) {
-                walkSpeed = 5;
-                luigiSpriteNode.setAnimation(luigiRunAnimation);
+            if (walkSpeed > 1) {
+                if (!prevSprint) {
+                    prevSprint = true;
+                    luigiSpriteNode.setAnimation(luigiRunAnimation);
+                }
             }
             else {
-                walkSpeed = 2;
+                prevSprint = false;
             }
+        }
+        else if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE, ƒ.KEYBOARD_CODE.ARROW_UP])) {
         }
         else {
             luigiSpriteNode.showFrame(0);
             luigiSpriteNode.setAnimation(luigiWalkAnimation);
         }
         luigiSpriteNode.mtxLocal.rotation = ƒ.Vector3.Y(leftDirection ? 180 : 0);
-    }
-    async function luigiFall() {
-        let deltaTime = ƒ.Loop.timeFrameGame / 1000;
-        let y_Speed = gravity * deltaTime;
-        luigiSpriteNode.mtxLocal.translateY(y_Speed);
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
--- Luigi/Script/Source/Main.ts
+++ Luigi/Script/Source/Main.ts
@@ -4,48 +4,47 @@ namespace Script {
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
+  document.addEventListener("interactiveViewportStarted", <EventListener><unknown>start);
 
   //LuigiSprite
   let luigiSpriteNode: ƒAid.NodeSprite;
   let luigi: ƒ.Node;
-
-  //Speed and Direction Variables
-  let walkSpeed: number = 2;
-  let leftDirection: boolean;
-  let gravity : number = 5;
  
-  
+  async function start(_event: CustomEvent): Promise<void> {
+    viewport = _event.detail;
+    luigiNodeInit(_event);
+  }
+
   //Sprite Animations
   let luigiWalkAnimation: ƒAid.SpriteSheetAnimation;
   let luigiRunAnimation: ƒAid.SpriteSheetAnimation;
 
+  async function initAnimations (coat: ƒ.CoatTextured): Promise<void> {
+    luigiWalkAnimation = new ƒAid.SpriteSheetAnimation("luigi_walk", coat);
+    luigiWalkAnimation.generateByGrid(ƒ.Rectangle.GET(10, 60, 20, 45), 8, 50, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(20));
 
-  document.addEventListener("interactiveViewportStarted", <EventListener><unknown>start);
+    luigiRunAnimation = new ƒAid.SpriteSheetAnimation("luigi_run", coat);
+    luigiRunAnimation.generateByGrid(ƒ.Rectangle.GET(8, 245, 37, 45), 2, 50, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(40));
+  }
 
-  async function start(_event: CustomEvent): Promise<void> {
-    viewport = _event.detail;
-
+  async function update(_event: Event): Promise<void> {
     
+    //luigiFall();
+    luigiControls();
 
+    viewport.draw();
+    ƒ.AudioManager.default.update();
+  }
+
+  async function luigiNodeInit(_event:CustomEvent): Promise<void> {
     let graph: ƒ.Node = viewport.getBranch();
     luigi = graph.getChildrenByName("LuigiPosition")[0].getChildrenByName("Luigi")[0];
     luigi.getComponent(ƒ.ComponentMaterial).activate(false);
     luigiSpriteNode = await createluigiSprite();
     luigi.addChild(luigiSpriteNode);
-
-    luigiFall();
-
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-
     ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, 10);
-  }
-
-  async function update(_event: Event): Promise<void> {
     
-    luigiControls();
-
-    viewport.draw();
-    ƒ.AudioManager.default.update();
   }
 
   async function createluigiSprite(): Promise<ƒAid.NodeSprite> {
@@ -53,12 +52,8 @@ namespace Script {
     let luigiSpriteSheet: ƒ.TextureImage = new ƒ.TextureImage();
     await luigiSpriteSheet.load("modern_luigi_sprite_sheet_by_mbf1000_d86t2ex.png");
     let coat: ƒ.CoatTextured = new ƒ.CoatTextured(undefined, luigiSpriteSheet);
-
-    luigiWalkAnimation = new ƒAid.SpriteSheetAnimation("luigi_walk", coat);
-    luigiWalkAnimation.generateByGrid(ƒ.Rectangle.GET(10, 60, 20, 45), 8, 50, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(20));
-
-    luigiRunAnimation = new ƒAid.SpriteSheetAnimation("luigi_run", coat);
-    luigiRunAnimation.generateByGrid(ƒ.Rectangle.GET(8, 245, 37, 45), 2, 50, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(40));
+    initAnimations(coat);
+    
 
     luigiSpriteNode = new ƒAid.NodeSprite("luigi_Sprite");
     luigiSpriteNode.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
@@ -76,42 +71,79 @@ namespace Script {
     return luigiSpriteNode;
   }
 
+  //Speed and Direction Variables
+  const xSpeedDefault: number = .9;
+  const xSpeedSprint: number = 5;
+  let ySpeed: number = 0.1;
+  let gravity: number = 0.05;
+
+  let leftDirection: boolean;
+  let prevSprint: boolean = false;
+
   async function luigiControls(): Promise<void>{
-    let distance = walkSpeed * ƒ.Loop.timeFrameGame / 1000;
+    let deltaTime: number = ƒ.Loop.timeFrameGame / 1000;
+    ySpeed -= gravity * deltaTime;
+    luigiSpriteNode.mtxLocal.translateY(ySpeed);
+
+    let pos: ƒ.Vector3 = luigiSpriteNode.mtxLocal.translation;
+    if (pos.y + ySpeed > 0){
+    luigiSpriteNode.mtxLocal.translateY(ySpeed);
+    }
+    else {
+      ySpeed = 0;
+      pos.y = 0;
+      luigiSpriteNode.mtxLocal.translation = pos;
+    }
+
+
+    let walkSpeed: number = xSpeedDefault;
+    if (leftDirection) {
+      walkSpeed = -xSpeedDefault;
+    }
+
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SHIFT_LEFT, ƒ.KEYBOARD_CODE.SHIFT_RIGHT])) {
+      walkSpeed = xSpeedSprint;
+      if (leftDirection) {
+        walkSpeed = -xSpeedSprint;
+      }
+    }
+
+    const xTranslation: number = walkSpeed * deltaTime;
 
     if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT])) {
-      luigiSpriteNode.mtxLocal.translateX(-distance);
+      luigiSpriteNode.mtxLocal.translateX(-xTranslation);
       leftDirection = true;
-      if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SHIFT_LEFT, ƒ.KEYBOARD_CODE.SHIFT_RIGHT])) {
-        walkSpeed = -5;
-        luigiSpriteNode.setAnimation(luigiRunAnimation);
+      if (walkSpeed < -1) {
+        if (!prevSprint) {
+          prevSprint = true;
+          luigiSpriteNode.setAnimation(luigiRunAnimation);
+        }
       } else {
-        walkSpeed = -2;
+        prevSprint = false;
       }
 
     } else if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT])) {
-      luigiSpriteNode.mtxLocal.translateX(distance);
+      luigiSpriteNode.mtxLocal.translateX(xTranslation);
       leftDirection = false;
-      if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SHIFT_LEFT, ƒ.KEYBOARD_CODE.SHIFT_RIGHT])) {
-        walkSpeed = 5;
-        luigiSpriteNode.setAnimation(luigiRunAnimation);
+      if (walkSpeed > 1) {
+        if (!prevSprint) {
+          prevSprint = true;
+          luigiSpriteNode.setAnimation(luigiRunAnimation);
+        }
       } else {
-        walkSpeed = 2;
+        prevSprint = false;
       }
-    } else {
+    } else if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE, ƒ.KEYBOARD_CODE.ARROW_UP])) {
+      
+
+    }
+     else {
       luigiSpriteNode.showFrame(0);
       luigiSpriteNode.setAnimation(luigiWalkAnimation);
     }
 
     luigiSpriteNode.mtxLocal.rotation = ƒ.Vector3.Y(leftDirection ? 180:0);
     
-  }
-
-  async function luigiFall(): Promise<void>{
-     let deltaTime: number =  ƒ.Loop.timeFrameGame/1000;
-     let y_Speed: number = gravity * deltaTime;
-     
-    luigiSpriteNode.mtxLocal.translateY(y_Speed);
   }
 
 }
