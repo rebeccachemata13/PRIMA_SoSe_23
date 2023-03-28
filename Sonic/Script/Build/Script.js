bce02cf793962a042d56307a9107ee3eb6bb8a4b
Rebecca Chemata <80950540+rebeccachemata13@users.noreply.github.com>
2023-03-28 21:15:43 +0200

    Sonic Camera and First Movements

--- Sonic/Script/Build/Script.js
+++ Sonic/Script/Build/Script.js
@@ -1,1 +1,69 @@
-console.warn("%cFor scripting, start your favorite IDE (e.g. VSCode) with typescript installed, load and compile this project", "color: red; font-weight: bold;");
+"use strict";
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Project.registerScriptNamespace(Script); // Register the namespace to FUDGE for serialization
+    class CustomComponentScript extends ƒ.ComponentScript {
+        // Register the script as component for use in the editor via drag&drop
+        static iSubclass = ƒ.Component.registerSubclass(CustomComponentScript);
+        // Properties may be mutated by users in the editor via the automatically created user interface
+        message = "CustomComponentScript added to ";
+        constructor() {
+            super();
+            // Don't start when running in editor
+            if (ƒ.Project.mode == ƒ.MODE.EDITOR)
+                return;
+            // Listen to this component being added to or removed from a node
+            this.addEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+            this.addEventListener("nodeDeserialized" /* NODE_DESERIALIZED */, this.hndEvent);
+        }
+        // Activate the functions of this component as response to events
+        hndEvent = (_event) => {
+            switch (_event.type) {
+                case "componentAdd" /* COMPONENT_ADD */:
+                    ƒ.Debug.log(this.message, this.node);
+                    break;
+                case "componentRemove" /* COMPONENT_REMOVE */:
+                    this.removeEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+                    this.removeEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+                    break;
+                case "nodeDeserialized" /* NODE_DESERIALIZED */:
+                    // if deserialized the node is now fully reconstructed and access to all its components and children is possible
+                    break;
+            }
+        };
+    }
+    Script.CustomComponentScript = CustomComponentScript;
+})(Script || (Script = {}));
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Debug.info("Main Program Template running!");
+    let viewport;
+    let shadow;
+    let graph;
+    document.addEventListener("interactiveViewportStarted", start);
+    function start(_event) {
+        viewport = _event.detail;
+        graph = viewport.getBranch();
+        shadow = graph.getChildrenByName("Avatar")[0].getChildrenByName("Shadow")[0];
+        shadow.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4));
+        let cmpCamera = viewport.getBranch().getComponent(ƒ.ComponentCamera);
+        viewport.camera = cmpCamera;
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    }
+    function update(_event) {
+        // ƒ.Physics.simulate();  // if physics is included and used
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]))
+            shadow.mtxLocal.translateX(0.05);
+        else if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A])) {
+            shadow.mtxLocal.translateX(-0.05);
+            shadow.mtxLocal.rotateY(180);
+        }
+        viewport.draw();
+        // ƒ.AudioManager.default.update();
+    }
+})(Script || (Script = {}));
+//# sourceMappingURL=Script.js.map
--- Sonic/Script/Source/Main.ts
+++ Sonic/Script/Source/Main.ts
@@ -3,10 +3,19 @@ namespace Script {
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
+  let shadow: ƒ.Node;
+  let graph: ƒ.Node;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
+    graph = viewport.getBranch();
+    shadow = graph.getChildrenByName("Avatar")[0].getChildrenByName("Shadow")[0];
+    shadow.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4 ));
+    
+    let cmpCamera: ƒ.ComponentCamera = viewport.getBranch().getComponent(ƒ.ComponentCamera);
+    viewport.camera = cmpCamera;
+
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
@@ -14,7 +23,17 @@ namespace Script {
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
+    if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]))
+    shadow.mtxLocal.translateX(0.05);
+    else if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A])){
+    shadow.mtxLocal.translateX(-0.05);
+    shadow.mtxLocal.rotateY(180);
+    }
     viewport.draw();
-    ƒ.AudioManager.default.update();
+    
+   
+    // ƒ.AudioManager.default.update();
   }
+
 }
+
