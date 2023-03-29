2f9d864f2028bc12fe417a8b24bda43318305a32
rebeccachemata13 <80950540+rebeccachemata13@users.noreply.github.com>
2023-03-29 22:00:54 +0200

    Shadow jumps and Animation I guess....

--- Sonic/Script/Build/Script.js
+++ Sonic/Script/Build/Script.js
@@ -14,21 +14,21 @@ var Script;
             if (ƒ.Project.mode == ƒ.MODE.EDITOR)
                 return;
             // Listen to this component being added to or removed from a node
-            this.addEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
-            this.addEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
-            this.addEventListener("nodeDeserialized" /* NODE_DESERIALIZED */, this.hndEvent);
+            this.addEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
+            this.addEventListener("nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */, this.hndEvent);
         }
         // Activate the functions of this component as response to events
         hndEvent = (_event) => {
             switch (_event.type) {
-                case "componentAdd" /* COMPONENT_ADD */:
+                case "componentAdd" /* ƒ.EVENT.COMPONENT_ADD */:
                     ƒ.Debug.log(this.message, this.node);
                     break;
-                case "componentRemove" /* COMPONENT_REMOVE */:
-                    this.removeEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
-                    this.removeEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+                case "componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */:
+                    this.removeEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
+                    this.removeEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
                     break;
-                case "nodeDeserialized" /* NODE_DESERIALIZED */:
+                case "nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */:
                     // if deserialized the node is now fully reconstructed and access to all its components and children is possible
                     break;
             }
@@ -39,31 +39,85 @@ var Script;
 var Script;
 (function (Script) {
     var ƒ = FudgeCore;
+    var ƒAid = FudgeAid;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
     let shadow;
+    let shadowSpriteNode;
     let graph;
+    let shadowRunAnimation;
     document.addEventListener("interactiveViewportStarted", start);
-    function start(_event) {
+    async function start(_event) {
         viewport = _event.detail;
+        shadowNodeInit(_event);
+    }
+    async function initAnimations(coat) {
+        shadowRunAnimation = new ƒAid.SpriteSheetAnimation("Shadow_Run", coat);
+        shadowRunAnimation.generateByGrid(ƒ.Rectangle.GET(4, 63, 25, 44), 2, 90, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(30));
+    }
+    async function update(_event) {
+        shadowControls();
+        viewport.draw();
+    }
+    async function shadowNodeInit(_event) {
         graph = viewport.getBranch();
-        shadow = graph.getChildrenByName("Avatar")[0].getChildrenByName("Shadow")[0];
-        shadow.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4));
         let cmpCamera = viewport.getBranch().getComponent(ƒ.ComponentCamera);
         viewport.camera = cmpCamera;
-        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
-        ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+        shadow = graph.getChildrenByName("Avatar")[0].getChildrenByName("Shadow")[0];
+        shadowSpriteNode = await createShadowSprite();
+        shadow.addChild(shadowSpriteNode);
+        ƒ.Loop.addEventListener("loopFrame" /* ƒ.EVENT.LOOP_FRAME */, update);
+        ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, 30);
     }
-    function update(_event) {
-        // ƒ.Physics.simulate();  // if physics is included and used
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]))
-            shadow.mtxLocal.translateX(0.05);
-        else if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A])) {
-            shadow.mtxLocal.translateX(-0.05);
-            shadow.mtxLocal.rotateY(180);
+    async function createShadowSprite() {
+        // load spritesheet from folder and add a "coat" to it.
+        let shadowSpriteSheet = new ƒ.TextureImage();
+        await shadowSpriteSheet.load("./sprites/shadow_sprite_sheet.png");
+        let coat = new ƒ.CoatTextured(undefined, shadowSpriteSheet);
+        initAnimations(coat);
+        // add running animation
+        shadowSpriteNode = new ƒAid.NodeSprite("Shadow_Sprite");
+        shadowSpriteNode.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
+        shadowSpriteNode.setAnimation(shadowRunAnimation);
+        shadowSpriteNode.setFrameDirection(1);
+        return shadowSpriteNode;
+    }
+    const xSpeedDefault = .9;
+    let ySpeed = 0.1;
+    let gravity = 0.1;
+    let leftDirection;
+    const jumpForce = 0.08;
+    async function shadowControls() {
+        let deltaTime = ƒ.Loop.timeFrameGame / 1000;
+        ySpeed -= gravity * deltaTime;
+        shadowSpriteNode.mtxLocal.translateY(ySpeed);
+        let pos = shadowSpriteNode.mtxLocal.translation;
+        if (pos.y + ySpeed > 0) {
+            shadowSpriteNode.mtxLocal.translateY(ySpeed);
         }
-        viewport.draw();
-        // ƒ.AudioManager.default.update();
+        else {
+            ySpeed = 0;
+            pos.y = 0;
+            shadowSpriteNode.mtxLocal.translation = pos;
+        }
+        let walkSpeed = xSpeedDefault;
+        if (leftDirection) {
+            walkSpeed = -xSpeedDefault;
+        }
+        const xTranslation = walkSpeed * deltaTime;
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT])) {
+            shadowSpriteNode.mtxLocal.translateX(-xTranslation);
+            leftDirection = true;
+        }
+        else if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT])) {
+            shadowSpriteNode.mtxLocal.translateX(xTranslation);
+            leftDirection = false;
+        }
+        else if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE, ƒ.KEYBOARD_CODE.ARROW_UP]) && ySpeed === 0) {
+            shadowSpriteNode.mtxLocal.translation = new ƒ.Vector3(pos.x, 0, 0.001);
+            ySpeed = jumpForce;
+        }
+        shadowSpriteNode.mtxLocal.rotation = ƒ.Vector3.Y(leftDirection ? 180 : 0);
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
--- Sonic/Script/Source/Main.ts
+++ Sonic/Script/Source/Main.ts
@@ -1,39 +1,104 @@
 namespace Script {
   import ƒ = FudgeCore;
+  import ƒAid = FudgeAid;
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
   let shadow: ƒ.Node;
+  let shadowSpriteNode: ƒAid.NodeSprite;
   let graph: ƒ.Node;
-  document.addEventListener("interactiveViewportStarted", <EventListener>start);
+  let shadowRunAnimation: ƒAid.SpriteSheetAnimation;
 
-  function start(_event: CustomEvent): void {
-    viewport = _event.detail;
-    graph = viewport.getBranch();
-    shadow = graph.getChildrenByName("Avatar")[0].getChildrenByName("Shadow")[0];
-    shadow.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4 ));
+  document.addEventListener("interactiveViewportStarted", <EventListener><unknown>start);
+
+  async function start(_event: CustomEvent): Promise<void> {
     
+    viewport= _event.detail;
+    shadowNodeInit(_event);
+    
+  }
+
+  async function initAnimations(coat: ƒ.CoatTextured ): Promise<void>{
+    shadowRunAnimation = new ƒAid.SpriteSheetAnimation("Shadow_Run", coat);
+    shadowRunAnimation.generateByGrid(ƒ.Rectangle.GET(4, 63, 25, 44), 2, 90, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(30));
+  }
+
+  async function update(_event:Event): Promise<void>{
+    shadowControls();
+    viewport.draw();
+
+  }
+
+  async function shadowNodeInit(_event:CustomEvent):Promise<void>{
+    graph = viewport.getBranch();
     let cmpCamera: ƒ.ComponentCamera = viewport.getBranch().getComponent(ƒ.ComponentCamera);
     viewport.camera = cmpCamera;
-
-
-    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
-  }
-
-  function update(_event: Event): void {
-    // ƒ.Physics.simulate();  // if physics is included and used
-    if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]))
-    shadow.mtxLocal.translateX(0.05);
-    else if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A])){
-    shadow.mtxLocal.translateX(-0.05);
-    shadow.mtxLocal.rotateY(180);
-    }
-    viewport.draw();
+    shadow = graph.getChildrenByName("Avatar")[0].getChildrenByName("Shadow")[0];
     
-   
-    // ƒ.AudioManager.default.update();
+    shadowSpriteNode = await createShadowSprite();
+    
+    shadow.addChild(shadowSpriteNode);
+    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
+    ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, 30);
+    
   }
 
-}
+  async function createShadowSprite(): Promise<ƒAid.NodeSprite>{
+    // load spritesheet from folder and add a "coat" to it.
+    let shadowSpriteSheet: ƒ.TextureImage = new ƒ.TextureImage();
+    await shadowSpriteSheet.load("./sprites/shadow_sprite_sheet.png");
+    let coat: ƒ.CoatTextured = new ƒ.CoatTextured(undefined, shadowSpriteSheet);
+    initAnimations(coat);
 
+    // add running animation
+    shadowSpriteNode = new ƒAid.NodeSprite("Shadow_Sprite");
+    shadowSpriteNode.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
+    shadowSpriteNode.setAnimation(shadowRunAnimation);
+    shadowSpriteNode.setFrameDirection(1);
+    
+
+    return shadowSpriteNode;
+
+  }
+
+  const xSpeedDefault: number = .9;
+  let ySpeed: number = 0.1;
+  let gravity: number = 0.1;
+  let leftDirection: boolean;
+  const jumpForce = 0.08;
+
+  async function shadowControls(): Promise<void>{
+    let deltaTime:number = ƒ.Loop.timeFrameGame / 1000;
+    ySpeed -= gravity*deltaTime;
+    shadowSpriteNode.mtxLocal.translateY(ySpeed);
+
+    let pos: ƒ.Vector3 = shadowSpriteNode.mtxLocal.translation;
+    if(pos.y + ySpeed > 0){
+      shadowSpriteNode.mtxLocal.translateY(ySpeed);
+    }
+    else{
+      ySpeed = 0;
+      pos.y = 0;
+      shadowSpriteNode.mtxLocal.translation = pos;
+    }
+
+    let walkSpeed: number = xSpeedDefault;
+    if (leftDirection) {
+      walkSpeed =-xSpeedDefault;
+    }
+
+    const xTranslation: number = walkSpeed * deltaTime;
+
+    if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT])) {
+       shadowSpriteNode.mtxLocal.translateX(-xTranslation);
+       leftDirection = true;
+    }else if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT])){
+      shadowSpriteNode.mtxLocal.translateX(xTranslation);
+      leftDirection = false;
+    }else if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE, ƒ.KEYBOARD_CODE.ARROW_UP]) && ySpeed === 0){
+      shadowSpriteNode.mtxLocal.translation = new ƒ.Vector3(pos.x, 0, 0.001);
+      ySpeed = jumpForce;
+
+    }
+    shadowSpriteNode.mtxLocal.rotation = ƒ.Vector3.Y(leftDirection ? 180:0);
+}}
